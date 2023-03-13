a3990e439881ed9176fe755a7a5a75d2705af86f
Aiden Carpenter <acarpenter102@gmail.com>
2023-03-13 14:49:35 -0500

    push generated tests but actually

---
+++ src/generate-tests.py
@@ -0,0 +1,45 @@
+import random
+import numpy as np
+
+# Seeding random so that everyone gets the same tests
+random.seed(0)
+
+NUM_TESTS = 500
+TEST_SIZES = [7]
+
+def generate_test(filepath: str, grid_size: int):
+    test_grid = np.full(shape=(grid_size, grid_size), fill_value='.')
+
+    num_haybales = [0]*random.randint(grid_size+1, grid_size*2)
+    num_ponds = [0]*random.randint(2, grid_size//2)
+
+    for _i in num_haybales:
+        m = random.randint(0, grid_size-1)
+        n = random.randint(0, grid_size-1)
+
+        if test_grid[m][n] == '.':
+            test_grid[m][n] = '@'
+        else:
+            num_haybales.append(0)
+
+    for _i in num_ponds:
+        m = random.randint(0, grid_size-1)
+        n = random.randint(0, grid_size-1)
+
+        if test_grid[m][n] == '.':
+            test_grid[m][n] = '#'
+        else:
+            num_ponds.append(0)
+
+    with open(filepath, 'w') as test:
+        test.write(str(grid_size) + '\n')
+        for row in test_grid:
+            for char in row:
+                test.write(char)
+            test.write('\n')
+
+# Generate tests
+for size in TEST_SIZES:
+    for test_num in range(NUM_TESTS):
+        test_name = "src/tests/input/{}x{}test{}.txt".format(size, size, test_num)
+        generate_test(test_name, size)
