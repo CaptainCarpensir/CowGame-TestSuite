3a026c09388bbe4408da681fb004855f35ceb7d3
Aiden Carpenter <acarpenter102@gmail.com>
2023-03-22 14:49:23 -0500

    update run.sh to basics

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
+> src/output/times.txt
 for i in {0..499}
 do
-    python3 src/verify-solution.py src/tests/input/7x7test$i.txt "src/tests/output/7x7test$i-sol.txt"
+    (/usr/bin/time -f '\t%U user' $1 "src/tests/input/7x7test"$i".txt" "src/tests/output/7x7test"$i".txt") 2>> src/output/times.txt
 done
