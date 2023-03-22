ed1e2312ae3b22ddb9e3bcda07dd04b0d8171f09
Aiden Carpenter <amctyg@umsystem.edu>
2023-03-22 15:28:13 -0500

    have bash script output run time

--- run.sh
+++ run.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+# Create output filestructure
+mkdir src/output
 > src/output/times.txt
+> src/output/errors.txt
+
 for i in {0..499}
 do
-    (/usr/bin/time -f '\t%U user' $1 "src/tests/input/7x7test"$i".txt" "src/tests/output/7x7test"$i".txt") 2>> src/output/times.txt
+    # Take current time in milliseconds via date command
+    ts=$(date +%s%N)
+    # Run given executable
+    $1 "src/tests/input/7x7test"$i".txt" "src/tests/output/7x7test"$i"-sol.txt"
+    # Output time taken into file
+    echo "$((($(date +%s%N) - $ts)/1000000))" >> src/output/times.txt
+    # Verify solution correctness
+    python3 src/verify-solution.py "src/tests/input/7x7test"$i".txt" "src/tests/output/7x7test"$i"-sol.txt" 2>> src/output/errors.txt
 done
